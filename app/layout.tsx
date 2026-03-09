cce92ee0d8f3e78a16e52e25a205c696f4330006
tt <xiaotian3303@outlook.com>
2026-03-09 22:01:59 +0800

    navbar和首页

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navibar from "@/components/navibar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ClerkProvider>{children}</ClerkProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-row`}
+        >
+          <div className="w-1/5 h-screen bg-gray-50">
+            <Navibar />
+          </div>
+          <div className="w-4/5 h-screen">{children}</div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ components/navibar.tsx
@@ -0,0 +1,9 @@
+const Navibar = () => {
+  return (
+    <p className="h-full w-2/3 bg-blue-100 rounded-lg flex items-center justify-center font-thin">
+      创建新的对话
+    </p>
+  );
+};
+
+export default Navibar;
